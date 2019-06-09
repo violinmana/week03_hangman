759545d1a6c441958de30814409689da6638bde0
WoF <violinmana@gmail.com>
2019-06-09 13:14:37 -0700

    It works, not sure if final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,99 @@
 //Define Variables
-var wordsArr = ['blizzard', 'diablo', 'warcraft', 'starcraft', 'overwatch']
+var wordsArr = ['arika', 'atlus', 'bandai', 'namco', 'camelot', 'capcom', 'cavia', 'chunsoft', 'clover', 'compile', 'cyberconnect', 'feelplus', 'fromsoftware', 'gamefreak', 'grezzo', 'ideafactory', 'hudsonsoft', 'intelligent', 'kairosoft', 'koeitemco', 'kojimaproductions', 'konami', 'levelfive', 'marvelous', 'mistwalker', 'monolithsoft', 'nintendo', 'nipponichi', 'omegaforce', 'platinum', 'silicon', 'squareenix', 'teamninja', 'triace', 'grindinggear', 'massive', 'machine', 'paradox', 'activision', 'arenanet', 'bethesda', 'blizzard', 'bungie', 'crystaldyn', 'demiurge', 'ensemble', 'gearbox', 'highnoon', 'infinityward', 'insomniac', 'irrational', 'lucasarts', 'microprose', 'monolith', 'netherrealm', 'midway', 'redstorm', 'respawn', 'westwood', 'obsidian', 'psyonix', 'sierra', 'stainless', 'telltale', 'visceral', 'valve', 'turbine']
 var solutionArr = []
 var inputArr = []
 var triesCounter = 0
 var wrongLetters = []
 var passed = false
 
-//Functions to Restart, Initialize, Update DOM
+var buttonValue = ''
+
+//Functions to Initialize, Update, RestartDOM
+
+var initializePage = function () {
+    var solutionWord = wordsArr[Math.floor(Math.random() * wordsArr.length)]
+    solutionArr = solutionWord.split('')
+    for (var i = 0; i < solutionArr.length; i++) {
+        inputArr.push('_')
+    }
+    triesCounter = solutionArr.length
+
+    console.log(solutionWord)
+    console.log(solutionArr)
+}
+
+var updatePage = function () {
+    document.getElementById('user-input').innerHTML = inputArr.join(' ')
+    document.getElementById('user-input-wrong').innerHTML = wrongLetters.join(' ')
+    document.getElementById('guesses-left').innerHTML = triesCounter
+
+    if (passed === true) {
+        document.getElementById('pass-or-fail').innerHTML = 'Congratulations!'
+    } else if (triesCounter > 0) {
+        document.getElementById('pass-or-fail').innerHTML = 'Click on another letter.'
+    } else {
+        document.getElementById('pass-or-fail').innerHTML = 'Wow, what a Failure.'
+    }
+
+    console.log(inputArr)
+}
+
 var restartPage = function () {
     solutionArr = []
     inputArr = []
     triesCounter = 0
     wrongLetters = []
     passed = false
+    initializePage()
+    updatePage()
 }
 
-var initializePage = function () {
-    solutionArr = str.split(wordsArr[Math.ceil(Math.random() * wordsArr.length)])
-    for (var i = 0; i < solutionArr.length; i++){
-        inputArr.push('_')
+// Function to Check for Win
+var winCheck = function (e) {
+    var numIncorrect = 0
+    for (var i = 0; i < inputArr.length; i++) {
+        if (inputArr[i] === '_') {
+            numIncorrect++
+        }
+    }
+    if (numIncorrect != 0) {
+        return false
+    } else {
+        return true
     }
 }
 
-var updatePage = function () {
-    document.getElementById('user-input').innerHTML = inputArr.join('_')
-    document.getElementById('user-input-wrong').innerHTML = wrongletters.join('_')
-    document.getElementById('guesses-left').innerHTML = triesCounter
+//Initial initalization
+initializePage()
+updatePage()
 
-    if (triesCounter === 0 && passed === false) {
-        document.getElementById('pass-or-fail').innerHTML = 'Wow, what a Failure.'
-    } else if (triesCounter > 0) {
-        document.getElementById('pass-or-fail').innerHTML = 'Click on another letter.'
+var onBtnClick = function (e) {
+
+    var clickedBtn = e
+    var numIncorrect = 0
+    console.log(clickedBtn)
+
+    if (passed) {
+
     } else {
-        document.getElementById('pass-or-fail').innerHTML = 'Congratulations!'
+        if (triesCounter != 0) {
+
+            for (var i = 0; i < solutionArr.length; i++) {
+                if (e === solutionArr[i]) {
+                    inputArr[i] = solutionArr[i]
+                } else {
+                    numIncorrect++
+                }
+            }
+
+            if (numIncorrect === solutionArr.length) {
+                triesCounter--
+            }
+
+            passed = winCheck()
+
+        }
+        updatePage()
+        console.log(passed)
     }
 }
